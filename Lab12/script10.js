d17163c8a4f3db4a35443d81efc8c15f8e1acc9d
Marco Salas <marcolex30@gmail.com>
2025-10-27 16:58:29 -0500

    Agrega script10.js

---
+++ Lab12/script10.js
@@ -0,0 +1,5 @@
+const saludo = nombre => `Hola, ${nombre}! Bienvenido`;
+
+console.log(saludo("Ana")); // "Hola, Ana! Bienvenido"
+console.log(saludo("Luis")); // "Hola, Luis! Bienvenido"
+console.log(saludo("María")); // "Hola, María! Bienvenido"
