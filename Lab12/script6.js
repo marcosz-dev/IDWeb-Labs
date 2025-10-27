cbbee5efe9c5385796c04f04b78335e2619a2900
Marco Salas <marcolex30@gmail.com>
2025-10-27 16:41:34 -0500

    Agrega script6.js

---
+++ Lab12/script6.js
@@ -0,0 +1,7 @@
+function mayorDeTres(a, b, c) {
+    return Math.max(a, b, c);
+}
+
+console.log(mayorDeTres(10, 5, 8)); // 10
+console.log(mayorDeTres(3, 7, 2));  // 7
+console.log(mayorDeTres(1, 1, 1));  // 1
