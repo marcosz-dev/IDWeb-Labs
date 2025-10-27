1dfac09177b984f2957425ac2f690f4c49084800
Marco Salas <marcolex30@gmail.com>
2025-10-27 16:49:03 -0500

    Agrega script7.js y script8.js

---
+++ Lab12/script7.js
@@ -0,0 +1,7 @@
+const promedio = function(nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+};
+
+console.log(promedio(7, 8, 9)); // 8
+console.log(promedio(5, 6, 7)); // 6
+console.log(promedio(10, 10, 10)); // 10
---
+++ Lab12/script8.js
@@ -0,0 +1,7 @@
+const convertirAMayusculas = function(str) {
+    return str.toUpperCase();
+};
+
+console.log(convertirAMayusculas("hola")); // "HOLA"
+console.log(convertirAMayusculas("mundo")); // "MUNDO"
+console.log(convertirAMayusculas("JavaScript")); // "JAVASCRIPT"
