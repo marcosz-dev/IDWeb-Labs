a57c04182eb2a3bff70250b878a4b67a8dc4f42c
Marco Salas <marcolex30@gmail.com>
2025-10-27 16:32:53 -0500

    Agrega script4.js

---
+++ Lab12/script4.js
@@ -0,0 +1,6 @@
+function areaRectangulo(base, altura) {
+    return base * altura;
+}
+
+console.log("Área del rectángulo de base 5 y altura 10: " + areaRectangulo(5, 10));
+console.log("Área del rectángulo de base 7 y altura 3: " + areaRectangulo(7, 3));
